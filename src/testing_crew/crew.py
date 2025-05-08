90c55e80ca364f4cac0ba484514458079528cad9
Saurabh <srksaurzz@gmail.com>
2025-05-08 13:30:47 +0530

    Updating agents

--- src/testing_crew/crew.py
+++ src/testing_crew/crew.py
@@ -1,29 +1,50 @@
-from crewai import Agent, Crew, Process, Task
-from crewai.project import CrewBase, agent, crew, task
+from crewai import Agent, Crew, Process, Task, LLM
+from crewai.project import CrewBase, agent, crew, task, before_kickoff, after_kickoff
 from crewai.agents.agent_builder.base_agent import BaseAgent
 from typing import List, Dict
+import yaml
+from crewai_tools import SerperDevTool
 
 @CrewBase
 class TestingCrew():
-    """TestingCrew crew"""
-    
-    agents_config: Dict[str, dict]
-    tasks_config: Dict[str, dict]
-    agents: List[BaseAgent]
-    tasks: List[Task]
+    def __init__(self):
+        self.llmGemini = LLM(model="gemini/gemini-1.5-flash",
+                       temperature=0.7,)   
+            
+        self.agents_config: Dict[str, dict]
+        self.tasks_config: Dict[str, dict]
+        self.agents: List[BaseAgent]
+        self.tasks: List[Task]
+        
+        filepath="src/testing_crew/config/config.yaml"
+        with open(filepath, 'r') as f:
+            config = yaml.safe_load(f)
+        
+        self.company_name = config['company_name']
+        self.tool_google_search = SerperDevTool(
+            name=self.company_name,
+            n_results=3
+        )
+
+    @before_kickoff
+    def before_kickoff_function(self, inputs):
+        print(f"Before kickoff function with inputs: {inputs}")
+        return inputs # You can return the inputs or modify them as needed
 
     @agent
     def researcher(self) -> Agent:
         return Agent(
             config=self.agents_config['researcher'], 
-            verbose=True
-        )
+            verbose=True, 
+            llm=self.llmGemini,
+            tools=[self.tool_google_search])
 
     @agent
     def reporting_analyst(self) -> Agent:
         return Agent(
             config=self.agents_config['reporting_analyst'], 
-            verbose=True
+            verbose=True,
+            llm=self.llmGemini,
         )
 
     @task
@@ -54,5 +75,10 @@ class TestingCrew():
             agents=self.agents,
             tasks=self.tasks, 
             process=Process.sequential,
-            verbose=True
-        )
+            verbose=True)
+            
+    @after_kickoff
+    def after_kickoff_function(self, result):
+        print(f"After kickoff function with result: {result}")
+        return result # You can return the result or modify it as needed
+        
--- src/testing_crew/main.py
+++ src/testing_crew/main.py
@@ -9,7 +9,6 @@ import ast
 from crewai import Crew
 from testing_crew.crew import TestingCrew
 
-# Load environment variables
 load_dotenv()
 
 def load_config(filepath="src/testing_crew/config/config.yaml"):
@@ -26,63 +25,37 @@ def fetch_data(company_name):
         )
 
         results = tool.run(search_query=company_name)
-        return results  # Now returning the results
+        return results
 
     except Exception as e:
         print(f"Error fetching data from SerpAPI: {e}")
         return None
     
 def parse_data(data):
-    query = data['searchParameters']['q']
-    location = data['searchParameters']['location']
+    # query = data['searchParameters']['q']
+    # location = data['searchParameters']['location']
     top_results = data['organic']
-    related_searches = [item['query'] for item in data['relatedSearches']]
-    credits_used = data['credits'] 
+    # related_searches = [item['query'] for item in data['relatedSearches']]
+    # credits_used = data['credits'] 
     
     return top_results  
-
-def analyze_data(data, reporting_analyst):
-    print("Data Analysis:")
-    analysis_result = reporting_analyst.run(data=data)
-    print("Analysis Result:", analysis_result)
-    return analysis_result
-
-def generate_brand_summary(company_name, analysis_result):
-    summary = f"Brand Summary for {company_name}:\n"
-    summary += f"{analysis_result}\n"
-    summary += "--- End of Summary ---"
-    return summary
 
 def main():
     config = load_config()
     company_name = config['company_name']
 
-    # Initialize the crew
+    data = fetch_data(company_name)
+    
+    if data:
+        parsed_data = parse_data(data)
+    
     crew = TestingCrew()
     my_crew = crew.crew()
 
-    researcher = my_crew.agents[0]
-    reporting_analyst = my_crew.agents[1]
+    # researcher        = my_crew.agents[0]
+    # reporting_analyst = my_crew.agents[1]
     
-    data = fetch_data(company_name)
-
-    if data:
-        print(data)
-        parsed_data = parse_data(data)
-        
-        analysis_result = analyze_data(parsed_data, reporting_analyst)
-        
-        brand_summary = generate_brand_summary(company_name, analysis_result)
-        print(brand_summary)
-
-        df = pd.DataFrame([{'Company Name': company_name, 'Brand Summary': brand_summary}])
-        csv_filepath = 'brand_summary.csv'
-        file_exists = os.path.isfile(csv_filepath)
-
-        df.to_csv(csv_filepath, mode='a', header=not file_exists, index=False)
-        print(f"Brand summary appended to {csv_filepath}")
-    else:
-        print("Failed to retrieve data.")
-
+    my_crew.kickoff(inputs={'topic': company_name})
+    
 if __name__ == "__main__":
     main()
